32a3f10c8b3e4817151637962d596b97d1ccf11e
6xDevelopment <126029462+6xDev@users.noreply.github.com>
2023-02-24 12:11:54 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,8 @@ Config = {}
 Config.BlipName = "Robbery location"
 Config.Dispatch = "ps-dispatch"
 
+Config.Night = {20, 4} -- Players can rob only from 20:00 to 04:00.
+
 Config.Ped = "a_m_y_business_03"
 Config.PedHeading = 357.7963
 
