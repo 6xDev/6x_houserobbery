a3d37eb688844ef30467bced7a72fafc95d22bb8
6xDevelopment <126029462+6xDev@users.noreply.github.com>
2023-03-10 14:20:45 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = {}
 
 Config.BlipName = "Robbery location"
 Config.Dispatch = "ps-dispatch"
+Config.UseTarget = true -- Set false if you dont use qb-target[with target is resmon always 0.00!]
 
 Config.Night = {20, 4} -- Players can rob only from 20:00 to 04:00.
 Config.Phone = "high_phone" -- qb-phone, gks-phone, qs-phone, lb-phone, high_phone
